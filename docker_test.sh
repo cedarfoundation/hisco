b326f69b7ba2b12770da430076c0ee9295d18732
junkka <johan.junkka@gmail.com>
2015-06-25 13:07:35 +0200

    Move docker tests

--- docker_test.sh
+++ docker_test.sh
@@ -1,8 +1,8 @@
 # clean up
-rm docker_test/hisco*.tar.gz
+rm tests/docker_test/hisco*.tar.gz
 # run build of package
-Rscript -e 'devtools::build(path = "docker_test")'
+Rscript -e 'devtools::build(path = "tests/docker_test")'
 # cp to docker dir
-mv docker_test/hisco*.tar.gz docker_test/hisco.tar.gz
+mv tests/docker_test/hisco*.tar.gz tests/docker_test/hisco.tar.gz
 # build docker test
-docker build -t junkka/hiscotest docker_test > log.txt
+docker build -t junkka/hiscotest tests/docker_test > log.txt
